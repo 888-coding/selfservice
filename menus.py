c9df63cb20b588466a0ce0bd434f65f479f15219
888-coding <pachelee@gmail.com>
2026-04-16 07:02:35 -0300

    Melhorando menu

--- menus.py
+++ menus.py
@@ -1,4 +1,5 @@
 # Arquivos de Menu 
+import os 
 
 def company_title():
     for i in range(30):
@@ -12,6 +13,8 @@ def company_title():
             print("\n", end="")
 
 def menu_main():
+    os.system("cls")
+    company_title()
     menu_main_list = ['Vender', 'Produtos', 'Relatorios']
     for i in range(len(menu_main_list)):
         print(i+1, menu_main_list[i])
@@ -28,17 +31,21 @@ def menu_main():
 
 def menu_products():
     os.system("cls")
-    
+    company_title()
     menu_products_list = ['Cadastrar', 'Alterar', 'Mostrar todos']
     for i in range(len(menu_products_list)):
         print(i+1, menu_products_list[i])
     
+    print("0 Voltar")
     print("\n", end="")
     while True:
         input_selected = input("> Escolhe uma opção :  ")
         if int(input_selected) > len(menu_products_list):
             print("Erro")
         else:
+            if input_selected == "0":
+                menu_main()
+
             break
 
 

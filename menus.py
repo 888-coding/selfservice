d24beea3cb49c9c459b419f7287a494fb2c8f96d
888-coding <pachelee@gmail.com>
2026-04-16 06:55:49 -0300

    Alterando para usar com menus.py

--- menus.py
+++ menus.py
@@ -23,11 +23,22 @@ def menu_main():
             print("Erro")
         else:
             if input_selected == "2":
-                from products import print_products_menu
-                print_products_menu()
+                menu_products()
             break
 
 def menu_products():
-    pass
+    os.system("cls")
+    
+    menu_products_list = ['Cadastrar', 'Alterar', 'Mostrar todos']
+    for i in range(len(menu_products_list)):
+        print(i+1, menu_products_list[i])
+    
+    print("\n", end="")
+    while True:
+        input_selected = input("> Escolhe uma opção :  ")
+        if int(input_selected) > len(menu_products_list):
+            print("Erro")
+        else:
+            break
 
 

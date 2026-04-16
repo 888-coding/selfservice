f8bc8dfc9cc7cee69dc16c163efef1e5687b5098
888-coding <pachelee@gmail.com>
2026-04-16 06:49:22 -0300

    Alterando para usar com menus.py

--- products.py
+++ products.py
@@ -1,1 +1,19 @@
 # Products 
+import sqlite3 as s 
+import os 
+
+def print_products_menu():
+    os.system("cls")
+    
+    menu_products_list = ['Cadastrar', 'Alterar', 'Mostrar todos']
+    for i in range(len(menu_products_list)):
+        print(i+1, menu_products_list[i])
+    
+    print("\n", end="")
+    while True:
+        input_selected = input("> Escolhe uma opção :  ")
+        if int(input_selected) > len(menu_products_list):
+            print("Erro")
+        else:
+            break
+

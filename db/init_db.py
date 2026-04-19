8c7d910abb5961c8de9a3af7121902faacb2b230
888-coding <pachelee@gmail.com>
2026-04-19 00:11:41 -0300

    Criando o create table no init_db

--- db/init_db.py
+++ db/init_db.py
@@ -1,1 +1,19 @@
 # Init database 
+
+from db.connection import get_connection
+
+def init_bd():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        code TEXT NOT NULL, 
+        nome TEXT NOT NULL, 
+        preco INT 
+    )
+    """)
+
+
+    conn.commit()
+    conn.close()

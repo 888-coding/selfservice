d4dc3c6cfea43d53d79d2abf293696eeb90fd0ad
888-coding <pachelee@gmail.com>
2026-04-18 23:53:19 -0300

    Alterando Conexao de banco de dados

--- db/connection.py
+++ db/connection.py
@@ -1,2 +1,11 @@
 # Connection 
 
+import sqlite3 as s 
+import os 
+
+def get_connection():
+    database_name = "panda.db"
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(base_dir, database_name)
+
+    connection = s.connect(db_path)

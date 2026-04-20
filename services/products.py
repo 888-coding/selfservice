8378b6c2a7ebd0be68abc643e989c7597b8deb04
888-coding <pachelee@gmail.com>
2026-04-20 00:27:03 -0300

    Renovando products

---
+++ services/products.py
@@ -0,0 +1,15 @@
+# Products 
+from db.connection import get_connection
+
+def create_product():
+    pass
+
+def search_all_products():
+    pass
+
+def change_product_name():
+    pass
+
+def change_product_price():
+    pass
+

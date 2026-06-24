77488717984e524517f692b2c54ba95bb2a21ccc
888-coding <pachelee@gmail.com>
2026-06-24 01:07:05 -0300

    Criando menu principal

--- telas/tela_principal.py
+++ telas/tela_principal.py
@@ -3,11 +3,23 @@ import os
 
 def telaPrincipal():
     os.system("clear")
-    print("Hello estou na tela")
+    print("SISTEMA SELFSERVICE --- VERSÃO PRIMEIRO")
+    print("---------------------------------------")
+    print("1. PRODUTOS ")
+    print("2. VENDER")
+    print("3. RELATORIOS")
+    print("4. SAIR")
+
     while True:
         input_value = input("Digite algo : ")
 
-        if input_value == "hello":
+        if input_value == "1":
+            pass
+        elif input_value == "2":
+            pass
+        elif input_value == "3":
+            pass
+        elif input_value == "4":
             break
 
     pass

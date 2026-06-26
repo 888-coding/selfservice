3effab600bd0011e8d8945475aa57e44d5012915
888-coding <pachelee@gmail.com>
2026-06-26 09:16:32 -0300

    Validando as entradas digitadas

--- telas/tela_produtos.py
+++ telas/tela_produtos.py
@@ -29,23 +29,29 @@ def cadastrar_produto():
         os.system("clear")
         print("CADASTRO - PRODUTO")
         print("------------------")
-        codigo_produto = input("Codigo do produto : ")
-        if not codigo_produto:
-            input("Erro.. favor digitar correto")
-        else:
-            print(f"O codigo digitado é : {codigo_produto} ")
-            input("Digite enter para continuar ...")
-        nome_produto = input("Nome do produto : ")
-        if not nome_produto:
-            input("Erro .. favor digitar correto")
-        else:
-            print(f"O nome digitado é : {nome_produto} ")
-            input("Digite enter para continuar ...")
-        preco_produto = input("Preço do produto : ")
-        if not preco_produto:
-            input("Erro.. favor digitar correto")
-        else:
-            print(f"O preço digitado é : {preco_produto}")
-            input("Digite enter para continuar ...")
+        while True:
+            codigo_produto = input("Codigo do produto : ")
+            if not codigo_produto:
+                input("Erro.. favor digitar correto")
+            else:
+                print(f"O codigo digitado é : {codigo_produto} ")
+                input("Digite enter para continuar ...")
+                break
+        while True:    
+            nome_produto = input("Nome do produto : ")
+            if not nome_produto:
+                input("Erro .. favor digitar correto")
+            else:
+                print(f"O nome digitado é : {nome_produto} ")
+                input("Digite enter para continuar ...")
+                break
+        while True:
+            preco_produto = input("Preço do produto : ")
+            if not preco_produto:
+                input("Erro.. favor digitar correto")
+            else:
+                print(f"O preço digitado é : {preco_produto}")
+                input("Digite enter para continuar ...")
+                break
 
         break 

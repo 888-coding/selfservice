ff4dbf537f6dac015a9309023c3da1356c4fdd28
888-coding <pachelee@gmail.com>
2026-07-19 10:17:04 -0300

    Adicionando teste de Lista de Python que estou tentando entender

--- telas/tela_vender.py
+++ telas/tela_vender.py
@@ -48,10 +48,12 @@ def telaVenderCadastrar():
             # Se nao deseja adicionar mais, 
             # Continua para proximo passo 
     print("Pedido feito : ")
+    print(f"Data : {input_data}")
     for codigo, quantidade in lista_produtos:
-        print(f"Codigo do produto {codigo} Quantidade : {quantidade} ")
+        print(f"Codigo do produto {codigo} | Quantidade : {quantidade} ")
     input("Digite enter para continuar ...")
-
-
+    print("OBS: Imprindo a lista para entender :")
+    print(lista_produtos)
+    input("Continuar ... ")
 
 

20138a130f768069f253ae0f20c33be3fae646e7
888-coding <pachelee@gmail.com>
2026-07-27 09:12:46 -0300

    Criando conexao

--- db/get_connection.py
+++ db/get_connection.py
@@ -4,16 +4,21 @@ import os
 def conectar():
     # nome do arquivo 
     my_database = 'pandaDB.db'
+
     # Localizar meu diretorio atual
     script_dir = os.path.dirname(os.path.abspath(__file__))
+    
     # Adicionar a pasta 'db'
     pasta_db = os.path.join(script_dir, "db")
+    
     # Garantir que tenha a pasta (caso nao tem , ele cria)
     os.makedirs(pasta_db, exist_ok=True)
 
     # Juntar a pasta e o arquivo 
     db_path = os.path.join(pasta_db, my_database)
+    
     # conectar
     con = sqlite3.connect(db_path)
+    
     return con
 

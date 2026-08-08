77084596972040e4a76f2bb1d42336033f6c9bed
888-coding <pachelee@gmail.com>
2026-08-08 10:40:04 -0300

    Apagando arquivo teste

--- main2.py
+++
@@ -1,9 +0,0 @@
-from db.get_connection import conectar as conexao
-import telas.tela_principal as tela_principal
-import telas.tela_produtos as tela_produtos
-import telas.tela_vender as tela_vender
-
-def main():
-  tela_principal.telaPrincipal()
-  
-main()

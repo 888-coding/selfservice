d9ffc40fd4216adb2f951b099a874dfd2772d3b6
888-coding <pachelee@gmail.com>
2026-08-08 08:28:13 -0300

    Fazendo função de procurar codigo de produto

--- services/services_produtos.py
+++ services/services_produtos.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import time
 from db.get_connection import conectar as connection
 
 def procurar_todos_produtos():
@@ -12,6 +13,23 @@ def procurar_todos_produtos():
         finally:
             cur.close()
         return dados
+def procurar_codigo(dado):
+    codigo_a_procura = dado
+    with connection() as con :
+        cur = con.cursor()
+        try:
+            script = "SELECT * FROM products WHERE code = ? "
+            cur.execute(script, (codigo_a_procura,) )
+            dados = cur.fetchall()
+            if dados:
+                founded = True
+            else:
+                founded = False
+                dados = None
+
+        finally:
+            cur.close()
+        return (dados, founded)
 
 def adicionar_produto(dados):
     codigo = dados[0]
@@ -33,3 +51,31 @@ def adicionar_produto(dados):
             con.commit()
         finally:
             cur.close()
+
+def alterar_produto_nome(dado):
+    codigo = dado
+    founded = False
+
+    with connection() as con :
+        cur = con.cursor()
+        try:
+            script = "SELECT id, code, name FROM products WHERE code = ? "
+            cur.execute(script, (codigo) )
+            dados = cur.fetchall()
+            if dados :
+                founded = True
+        finally:
+            cur.close()
+
+        if founded : 
+            print("Codigo encontrado ... ")
+            time.sleep(3)
+            for dados in dados :
+                print(dado)
+                time.sleep(1)
+        else:
+            print("Codigo nao foi encontrado ... \n Retornaremos para pagina anterior")
+            time.sleep(4)
+            
+
+

f903be93ad0425b05ea6adb124b812cd24b085c5
888-coding <pachelee@gmail.com>
2026-08-18 10:05:59 -0300

    Gravando cabecalho e tambem gravando looping de produtos.

--- services/services_vender.py
+++ services/services_vender.py
@@ -5,9 +5,50 @@ from db.get_connection import conectar as connection
 
 def service_venderCadastro(cabecalho, produtos):
     cabecalho = cabecalho
+    date = cabecalho[0]
+    discount = 0 
+    totalValue = 0
     produtos = produtos
 
+    # Passo 1: Inserir cabelho
+    with connection() as con:
+        cur = con.cursor()
+        try:
+            # 1. Inserir no cabecalho 
+            script = """INSERT INTO selling(
+            sellingDate,
+            discount,
+            totalValue
+          )VALUES (?, ?, ?)"""
+            cur.execute(script, (date, discount, totalValue,) )
 
+            id_selling = cur.lastrowid
+
+            con.commit()
+
+            # TODO: Tem que fazer Looping para cada produto inserido
+            # 2. Inserir no selling details 
+            script = """
+                INSERT INTO sellingDetails(
+                sellingId,
+                productId,
+                productPrice,
+                productQuantity
+                ) VALUES(?, ?, ?, ?)
+            """
+            for produto in produtos :
+                productId = xx
+                productPrice = xx
+                productQuantity = xx
+                cur.execute(script, (id_selling, productId, productPrice, productQuantity,) ) 
+                con.commit() 
+
+        finally:
+            cur.close()
+
+    # Passo 2 Inserir produtos na tabela de selling sellingDetails
+    print(f"Id de selling criado é {id_selling} ")
+    
 # ??
 def service_venderAdicionarCabecalho():
     pass
